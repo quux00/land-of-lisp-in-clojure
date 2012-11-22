19d53690856766b28e9455534452f24339ecda51
Michael <mpeterson2@gmail.com>
2012-11-22 10:10:11 -0500

    Finished ch. 7 graphing of wizard's adventure.
    The uedges->dot fn needs to be refactored and unified with
    the edges->dot and related fns before calling this done.

--- wizards-adventure/src/thornydev/wizadv/graphing.clj
+++ wizards-adventure/src/thornydev/wizadv/graphing.clj
@@ -1,5 +1,7 @@
 (ns thornydev.wizadv.graphing
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]))
 
 (defn substitute-if [val predfn coll]
   (map #(if (predfn %) val %) coll))
@@ -7,7 +9,6 @@
 (comment (substitute-if 0 odd? [1 2 3 4 5 6 7 8]))
 (comment (substitute-if \e #(Character/isDigit %) "I'm a l33t hack3r!"))
 
-;; LIVING_ROOM->GARDEN[label="(WEST DOOR)"];
 (defn dot-name [kw]
   (str/upper-case (str/replace (name kw) "-" "_")))
 
@@ -16,14 +17,60 @@
     (str "(" body ")")))
 
 (defn edges->dot [edges] 
-  (doseq [start (keys edges)]
-    (doseq [[end vpath] (start edges)]
-      (do
-        (print (dot-name start))
-        (print "->")
-        (print (dot-name end))
-        (println (str "[label=\"" (dot-label vpath) "\"];")))
-      )))
+  (doseq [start       (keys edges)
+          [end vpath] (start edges)]
+    (print (dot-name start))
+    (print "->")
+    (print (dot-name end))
+    (println (str "[label=\"" (dot-label vpath) "\"];"))))
+
+(defn uedges->dot [edges]
+  (let [mkpair (fn [e1 e2]
+                       (map name (sort [e1 e2])))]
+    (loop [nodes (keys edges) pairs-seen #{}]
+      (if (empty? nodes)
+        pairs-seen
+        (let [start (first nodes)
+              vpairs (for [[end vpath] (start edges)]
+                       (let [pair (mkpair start end)]
+                         (do
+                           (when-not (pairs-seen pair)
+                             (print (dot-name start))
+                             (print "--")
+                             (print (dot-name end))
+                             (println (str "[label=\"" (dot-label vpath) "\"];")))
+                           pair)))]
+          (recur (rest nodes) (apply conj pairs-seen vpairs))
+          ))
+      ))
+  )
+
+(comment
+  (uedges->dot edges))
+
+(defn graph->dot [edges]
+  (println "digraph {")
+  (edges->dot edges)
+  (println "}"))
+
+(defn ugraph->dot [edges]
+  (println "graph {")
+  (uedges->dot edges)
+  (println "}"))
+
+(defn graph->svg [fname edges]
+  (with-open [w (io/writer fname)]
+    (binding [*out* w]
+      (graph->dot edges))
+    )
+  (sh "dot" "-Tsvg" fname "-o" "wizout.svg"))
+
+(defn ugraph->svg [fname edges]
+  (with-open [w (io/writer fname)]
+    (binding [*out* w]
+      (ugraph->dot edges))
+    )
+  (sh "dot" "-Tsvg" fname "-o" "uwizout.svg"))
 
 (comment
   (edges->dot edges))
