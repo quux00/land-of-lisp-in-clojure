603375be8b5d8ba46916200662f8d112aa8e1de5
Michael <mpeterson2@gmail.com>
2012-11-26 23:16:46 -0500

    Starting to flesh out the Monster protocol and records.
    Decided to follow the book and have methods/fns write to println.
    In the unit test I use with-out-str to redirect the output back
    so it can be parsed and tested.
    A fragile test strategy, but I'm learning some interesting features
    of Clojure this way and not sure the best way to handle it.

--- orc-battle/src/thornydev/orc/game.clj
+++ orc-battle/src/thornydev/orc/game.clj
@@ -11,13 +11,97 @@
                    :agility nil
                    :strength nil}))
 
+;; ---[ Monster setup and definitions ]--- ;;
+
 (def ^:dynamic *monsters* [])         ;; vector of monsters
 (def ^:dynamic *monster-builders* []) ;; vector
 (def ^:dynamic *monster-num* 12)      ;; # monsters per fight sequence
 
+(declare monster-dead? player-dead?)
+
+;; do- methods indicate non-pure functions that return
+;; void and are invoked to alter state (or just print to *out*
+(defprotocol Monster
+  "Generic super type for all the monsters in the orc-battle game"
+  (do-monster-show [this] "Describes the type of monster")
+  (monster-attack [this]
+    "Monster attacks player. Modifies player health.
+     @return string description of what happened.")
+  (monster-hit [this x]
+    "Player attacks/hits the monster.
+     @params
+       mon - monster to 'hit' (subtract health from)
+       x - amount of hit
+     @returns a new Monster with the new health value")
+  )
+
 ;; this macro auto-generates a factory function called
 ;; new-Monster that uses the defaults defined here
-(defrecord+defaults Monster [health (randval 10)])
+(defrecord+defaults Orc [health (randval 10) club-level (randval 8)]
+  Monster
+  (do-monster-show
+   [this]
+   (println (format "A wicked Orc with a level %d club and %d health",
+                    (:club-level this) (:health this))))
+  (monster-attack
+   [this]
+   (let [pts (randval (:club-level this))]
+     (swap! player update-in [:health] - pts)
+     (println "An orc swings his club at you and knocks off"
+              pts "of your health points"))
+   this)
+
+  ;; TODO: can this be made generic?
+  (monster-hit
+   [this x]
+   (let [orc (->Orc (- (:health this) x) (:club-level this))]
+     (if (monster-dead? orc)
+       (println "You killed the orc!")
+       (println "You hit the orc, knocking off" x "health points!"))
+     orc
+     ))
+  )
+
+(defrecord+defaults Hydra [health (randval 10)]
+  Monster
+  (do-monster-show
+   [this]
+   (println (format "A malicious hydra with %d heads" (:health this))))
+
+  (monster-attack
+   [this]
+   (let [x (randval (bit-shift-right (:health this) 1))]
+     (print "A hydra attacks you with" x "of its heads!")
+     (println "It also grows back one more head.")
+     (swap! player update-in [:health] - x)
+     (->Hydra (inc (:health this)))
+     ))
+
+  ;; TODO: can this be made generic?
+  (monster-hit
+   [this x]
+   (let [hyd (->Hydra (- (:health this) x))]
+     (if (monster-dead? hyd)
+       (println "The corpse of the fully decapitated hydra falls to the floor.")
+       (println "You lopped" x "of the hydra's heads!")
+       )
+     hyd))
+  )
+
+(comment
+  (binding [*monster-num* 12]
+    (init-player)
+    (let [orc (new-Orc)
+          hyd (new-Hydra)]
+      (do-monster-show orc)
+      (do-monster-show hyd)
+      (monster-attack orc)
+      (monster-attack orc)
+       
+      [@player]
+      ))
+  )
+
 
 (defn init-player []
   (swap! player merge {:health 30, :agility 2, :strength 30}))
@@ -43,20 +127,21 @@
 (defn stab-monster [monster])
 
 
-;; TODO: these need to be in a protocol or multi-method
-(defn monster-dead? [])
-(defn monster-attack [])
+(defn monster-dead? [mon]
+  (<= (:health mon) 0))
 
-(defn monster-hit
-  "@params
-     mon - monster to 'hit' (subtract health from)
-     x - amount of hit
-   @returns a new Monster with the new health value
-   Note that this fn will not modify the global list of monsters.
-   This fn returns a new monster that will need to slid into the
-   global vector of monsters"
-  [mon x]
-  (->Monster (- (:health mon) x)))
+;; TODO: does this need to go into the Monster protocol??
+;; (defn monster-hit
+;;   "@params
+;;      mon - monster to 'hit' (subtract health from)
+;;      x - amount of hit
+;;    @returns a new Monster with the new health value
+;;    Note that this fn will not modify the global list of monsters.
+;;    This fn returns a new monster that will need to slide into the
+;;    global vector of monsters"
+;;   [mon x]
+;;   ;; (->Monster (- (:health mon) x))
+;;   )
 
 (defn get-monster-choice
   "Looks up the monster from the *monster* vector
@@ -151,11 +236,13 @@
 
 (defn orc-battle
   "start the game"
-  []
-  (init-monsters)
-  (init-player)
-  (game-loop)
-  (cond
-   (player-dead?)   (println "You have been killed. Game over.")
-   (monsters-dead?) (println "Congratulations! You have vanquished your foes."))
+  [nmonsters]
+  (binding [*monster-builders* [new-Orc new-Hydra]]
+    (binding [*monster-num* nmonsters]
+      (init-monsters)
+      (init-player)
+      (game-loop)
+      (cond
+       (player-dead?)   (println "You have been killed. Game over.")
+       (monsters-dead?) (println "Congratulations! You have vanquished your foes."))))
   )
--- orc-battle/test/thornydev/orc/game_test.clj
+++ orc-battle/test/thornydev/orc/game_test.clj
@@ -20,6 +20,32 @@
 ;;                  (get-monster-choice :put-dead-monster-here)))
 ;;     ))
 
-(deftest monster-hit)
+(deftest test-Orc
+  (init-player)
+  
+  (let [orc (->Orc 8 5)]
+    (testing "show"
+      (let [show-out (with-out-str (do-monster-show orc))]
+        (is (re-find #"level 5 club" show-out))
+        (is (re-find #"8 health" show-out)))
+      )
+    (testing "attack"
+      (let [b4-player-strength (:health @player)
+            attack-out
+            (with-out-str
+              (let [mon (monster-attack orc)]
+                (is (= 8 (:health mon)))
+                (is (identical? orc mon))
+                ))
+            [_ hit] (first
+                     (re-seq #"knocks off (\d+) of your health" attack-out))]
+        ;; ensure current strength decreased by amount reported
+        ;; from the monster-attack method
+        (is (= (Integer/valueOf hit)
+               (- b4-player-strength (:health @player))))
+        )
+      )
+    )
+  )
 
 (prn (run-tests))
